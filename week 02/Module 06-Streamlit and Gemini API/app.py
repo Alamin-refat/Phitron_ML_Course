17f1159ee2707e5cf7d3db51009b0e7cb3305d1d
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-15 19:51:37 +0600

    api generate  complete

--- week 02/Module 06-Streamlit and Gemini API/app.py
+++ week 02/Module 06-Streamlit and Gemini API/app.py
@@ -8,3 +8,12 @@ load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
 
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents="Give me an idea of Gemoini API in 150 words"
+)
+
+
+st.markdown(response.text)
