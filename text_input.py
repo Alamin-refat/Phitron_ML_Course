3bd9583dfd3c1118710db09caf07a32123015459
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-15 13:21:33 +0600

    markdown

--- text_input.py
+++ text_input.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("My First Streamlit Web App", anchor=False)
+
+st.header("Content 1", divider=True)
+
+st.subheader("Content 2 subheader")
+
+st.text("Hello world!")
+
+st.markdown("**hello** *world*")
