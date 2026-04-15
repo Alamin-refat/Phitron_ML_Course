e3665d9cc7553edb6835a5a1bff20ae15fc79a9a
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-15 15:09:33 +0600

    button add and submitt

--- week 02/Module 06-Streamlit and Gemini API/input_element.py
+++ week 02/Module 06-Streamlit and Gemini API/input_element.py
@@ -6,10 +6,17 @@ st.divider()
 
 name = st.text_input("Enter your name")
 
-st.write("your name is: ",name)
+#st.write("your name is: ",name)
 
 st.divider()
 
 age = st.number_input("Enter your age", value=None,placeholder="type your age......")
 
-st.write("your age is: ",age)
+#st.write("your age is: ",age)
+
+presesed =st.button("Submit",type="primary")
+
+if presesed:
+    st.write(f"your name is: {name} and your age is: {age}")
+    
+
