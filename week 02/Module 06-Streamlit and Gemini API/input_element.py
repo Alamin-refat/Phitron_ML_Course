0bd253e6ca990cfcf32c3bd1a273a5b3cf814ad8
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-15 15:00:06 +0600

    placeholder use

--- week 02/Module 06-Streamlit and Gemini API/input_element.py
+++ week 02/Module 06-Streamlit and Gemini API/input_element.py
@@ -8,3 +8,8 @@ name = st.text_input("Enter your name")
 
 st.write("your name is: ",name)
 
+st.divider()
+
+age = st.number_input("Enter your age", value=None,placeholder="type your age......")
+
+st.write("your age is: ",age)
