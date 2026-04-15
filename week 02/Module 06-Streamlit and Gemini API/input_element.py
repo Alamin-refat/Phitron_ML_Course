104b5997628f1eaae0cbdfe527c5a1320d99b5cb
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-15 17:55:36 +0600

    select box

--- week 02/Module 06-Streamlit and Gemini API/input_element.py
+++ week 02/Module 06-Streamlit and Gemini API/input_element.py
@@ -16,6 +16,15 @@ age = st.number_input("Enter your age", value=None,placeholder="type your age...
 
 presesed =st.button("Submit",type="primary")
 
+
+selected =  st.selectbox("choose your profession",
+                         ("Student","Employee","Buisenessman"),
+                         index=None
+                         )
+
+
+st.write("your profession is: ",selected)
+
 if presesed:
     st.write(f"your name is: {name} and your age is: {age}")
     
