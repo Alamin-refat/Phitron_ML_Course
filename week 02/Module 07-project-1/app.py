1107dbd8030bd6fa4da013fd6ddd9602e0cc6215
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-16 18:32:37 +0600

    file uploader add

--- week 02/Module 07-project-1/app.py
+++ week 02/Module 07-project-1/app.py
@@ -7,4 +7,13 @@ st.title("Note Summary and Quiz Generator")
 st.markdown("Upload upto 3 images to generate Note summary and Quizzes")
 st.divider()
 
+# sidebar
 
+with st.sidebar:
+    st.header("Controls")
+    st.file_uploader(
+        "Upload the photos of your note",
+        type=["jpg", "jpeg", "png"],
+        accept_multiple_files=True,
+        
+    )
