56ec0a5db70a8ada071a2f69270ceb69d732a547
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-16 20:02:56 +0600

    add appi_calling file

--- week 02/Module 07-project-1/app.py
+++ week 02/Module 07-project-1/app.py
@@ -43,3 +43,43 @@ with st.sidebar:
         ("Easy", "Medium", "Hard"),
         index=None
     )
+    
+    
+    
+    # Generate button
+    
+    pressed = st.button("Click the button to initiate AI",type="primary")
+    
+    
+    
+if pressed:
+    if not images:
+        st.error("You must upload at least one image to generate the quiz.")
+        
+    if not selected_option:
+        st.error("You must select a difficulty level to generate the quiz.")
+        
+    if images and selected_option:
+        
+        # Note
+        with st.container(border=True):
+            st.subheader("Your Note")
+            
+            # The portion below will be replace by API call
+            st.text("Note will be shown here")
+        
+        
+        # Audio transcription
+        with st.container(border=True):
+            st.subheader("Audio transcription")
+            
+            # The portion below will be replace by API call
+            st.text("Audio transcription will be shown here")
+            
+        
+        # Quiz
+        with st.container(border=True):
+            st.subheader(f"Quiz ({selected_option}) Difficulty")
+            
+            # The portion below will be replace by API call
+            st.text("Quiz will be shown here")
