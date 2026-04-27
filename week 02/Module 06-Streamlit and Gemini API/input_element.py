47c4e68c36e71fae0a49a65adbcf13cad9afcbcd
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-27 20:10:26 +0600

    comment add

--- week 02/Module 06-Streamlit and Gemini API/input_element.py
+++ week 02/Module 06-Streamlit and Gemini API/input_element.py
@@ -4,12 +4,12 @@ st.title("Input your information",anchor=False)
 st.divider()
 name = st.text_input("Enter your name")
 
-#st.write("your name is: ",name)
+# st.write("your name is: ",name)
 
 st.divider()
 age = st.number_input("Enter your age", value=None,placeholder="type your age......")
 
-#st.write("your age is: ",age)
+# st.write("your age is: ",age)
 
 presesed =st.button("Submit",type="primary")
 
