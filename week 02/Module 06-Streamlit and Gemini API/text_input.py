5b6ef3bda2aa08d8cd6a211bbc6b7e83da7211a1
Alamin Refat <alaminrefat2017@gmail.com>
2026-04-27 20:13:15 +0600

    subheader add

--- week 02/Module 06-Streamlit and Gemini API/text_input.py
+++ week 02/Module 06-Streamlit and Gemini API/text_input.py
@@ -5,7 +5,6 @@ st.title("🗺  My First Streamlit Web App", anchor=False)
 st.header("Content 1", divider=True)
 
 st.subheader("Content 2 subheader")
-
 st.text("Hello world!")
 
 st.markdown(":red[**hello**] *world*")
